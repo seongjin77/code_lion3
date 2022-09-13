3ab3aa2ddb8e45f905cad2d0ae5b7ef49ea80832
seongjin <kssj9056@gmail.com>
2022-09-13 22:30:10 +0900

    css과제

--- 1만시간과제/js/10000hours.js
+++ 1만시간과제/js/10000hours.js
@@ -1,1 +1,9 @@
 // js file
+let click = document.querySelector('.btn-go');
+let btn = document.querySelector('.cont-modal');
+
+click.addEventListener('click',function(){
+console.log('ccc')
+btn.classList.toggle('togle-btn');
+
+});
