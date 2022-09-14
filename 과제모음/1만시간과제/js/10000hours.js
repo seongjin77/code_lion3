f6ff084dcffd33fc557258890116b208144bf6f5
seongjin <kssj9056@gmail.com>
2022-09-14 23:54:33 +0900

    merge

--- 과제모음/1만시간과제/js/10000hours.js
+++ 과제모음/1만시간과제/js/10000hours.js
@@ -3,7 +3,7 @@ let click = document.querySelector('.btn-go');
 let btn = document.querySelector('.cont-modal');
 
 click.addEventListener('click',function(){
-console.log('ccc')
+
 btn.classList.toggle('togle-btn');
 
 });
