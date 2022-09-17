a6d1496597d161b993eaa05e7c7992e43fe99cb6
seongjin <kssj9056@gmail.com>
2022-09-18 03:22:23 +0900

    css 과제

--- 바로지울것3.js
+++ 바로지울것3.js
@@ -1,10 +1,10 @@
 // let three = '10e22'
 let str = '3'
 let aaar = 3
-let strstr = 'e'
-let num = isNaN(strstr);
-// let change = parseInt(str);
+let strstr = '2e13';
+// let num = isNaN(strstr);
+let change = parseInt(strstr);
 
 
 
-console.log(num);
+console.log(change);
