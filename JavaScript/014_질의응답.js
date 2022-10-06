c944bc5e0fa34369a77987b4798bf1e26ed9dc55
seongjin <kssj9056@gmail.com>
2022-10-06 19:17:50 +0900

    수업자료

--- JavaScript/014_질의응답.js
+++
@@ -1,6 +0,0 @@
-console.log('hello world');
-console.log('hello world');
-
-let arrnum = [1,2,3,4,5];
-arrnum.splice(2);
-console.log(arrnum);
---
+++ test.js
@@ -0,0 +1,9 @@
+let x20 = ['8,88,888']
+let result20 = 0;
+for (let i = 0; i < x20.length; i++) {
+    if(x20[i] === '8'){
+        result20 += 1;
+    }
+}
+
+console.log(result20);
