52547563c842e2964f87b9883d5f703b7c5c2c9a
seongjin <kssj9056@gmail.com>
2022-10-11 17:15:57 +0900

    JavaScript

---
+++ JavaScript최신/test.js
@@ -0,0 +1,34 @@
+// function reverse(txt){
+//     if (txt.length ===1){
+//         return txt;
+//     }
+//     return reverse(txt.slice(1) + txt[0])
+// }
+
+// console.log(reverse('hello'));
+
+//////////////////////////////////////////
+// function reverse(txt) {
+//     if (txt.length === 1) {
+//       return txt;
+//     }
+//     return reverse(txt.slice(1)) + txt[0];
+//   }
+
+//   console.log(reverse('hello'));
+
+
+//////////////////////////////////////////
+
+  // 깔끔함, 사이트에 나와 있음 객체의 초깃값 
+// function func2({
+//     a = 2, 
+//     b = 1, 
+//     c = 3
+// } = {}) {
+//     console.log(a, b, c);
+//     return a + b + c;
+// }
+// console.log(func2({a: 20, b: 30, c: 10}));
+
+
