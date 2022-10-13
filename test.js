8f171e063e0b5ee1361fc64a310f53d26c6aac8d
seongjin <kssj9056@gmail.com>
2022-10-13 23:52:59 +0900

    오늘 수업

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
-let x20 = ['8,88,888']
-let result20 = 0;
-for (let i = 0; i < x20.length; i++) {
-    if(x20[i] === '8'){
-        result20 += 1;
-    }
-}
+let test = [...Array(100).keys()]
 
-console.log(result20);
+console.log(test);
